16c8a46f8eb57a7eb23d6f18122f2df91f41b615
lomuto <kangys1245@gmail.com>
2021-08-21 08:28:12 +0900

    Update calculation for contents

--- app/src/main/java/com/example/threeo/DetailActivity.kt
+++ app/src/main/java/com/example/threeo/DetailActivity.kt
@@ -42,19 +42,21 @@ class DetailActivity : AppCompatActivity() {
 //            val drawable = BitmapDrawable(resources, appIcon)
             iconImg.setImageResource(R.mipmap.ic_launcher_round)
             appTitle.text = appName
-            totalTime.text = "${allTime.toLong()/3600000}시간 ${(allTime.toLong()%3600000)/60000}분"
+            val hour = allTime.toLong()/3600000
+            val min = allTime.toLong()/60000 - hour*60
+            val totalMin = (hour*60 + min);
+            totalTime.text = "${hour}시간 ${min}분"
 
             //list를 관리하는 메니저 등록
             array.add("독서")
             array.add("달리기")
             array.add("음악")
             array.add("알바")
+            array.add("영화")
             array.add("영어단어")
             array.add("우주선")
-            array.add("계란")
             array.add("강의")
             array.add("탄소배출량")
-            array.add("쓰레기")
             array.add("바다거북이")
             array.add("나비")
             recyclerView2.layoutManager = LinearLayoutManager(this@DetailActivity, LinearLayoutManager.HORIZONTAL,false)
@@ -69,28 +71,28 @@ class DetailActivity : AppCompatActivity() {
                 ) {
                     when(adapter.items[position]){
                         "독서"->{
-                            resultVal.text = "${500} 권"
+                            resultVal.text = "${hour / 6} 권"
                         }
                         "달리기"->{
-                            resultVal.text = "${500} 칼로리"
+                            resultVal.text = "${totalMin * 9.6} 칼로리"
                         }
                         "음악"->{
-                            resultVal.text = "${500} 곡"
+                            resultVal.text = "${totalMin / 3} 곡"
                         }
                         "알바"->{
-                            resultVal.text = "${500} 원"
+                            resultVal.text = "${hour * 8720} 원"
+                        }
+                        "영화"->{
+                            resultVal.text = "${hour / 2} 편"
                         }
                         "영어단어"->{
-                            resultVal.text = "${500} 개"
+                            resultVal.text = "${totalMin / 1.3} 개"
                         }
                         "우주선"->{
-                            resultVal.text = "${500} 번 왕복"
-                        }
-                        "계란"->{
-                            resultVal.text = "${500} 번 부화"
+                            resultVal.text = "${totalMin / 30} 번 왕복"
                         }
                         "강의"->{
-                            resultVal.text = "${500} 주차 수강"
+                            resultVal.text = "${hour / 2} 주차 수강"
                         }
                     }
                 }
