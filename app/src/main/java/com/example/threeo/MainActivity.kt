bfa1e3818d61b576a0df49ec449db182aa4d9188
jiwoo <hjw6112@gmail.com>
2021-08-21 01:02:51 +0900

    get time

--- app/src/main/java/com/example/threeo/MainActivity.kt
+++ app/src/main/java/com/example/threeo/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.threeo
 
+import android.app.AppOpsManager
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Process
+import android.provider.Settings
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { view ->
+            if(!checkForPermission()) {
+                Toast.makeText(
+                    this, "권한 설정이 필요합니다.",
+                    Toast.LENGTH_LONG
+                ).show()
+                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            }else{
+                val usageStats = getAppUsageStats()
+                showAppUsageStats(usageStats)
+            }
+        }
+    }
+
+    private fun showAppUsageStats(usageStats: MutableList<UsageStats>) {
+        usageStats.sortWith(Comparator { right, left ->
+            compareValues(left.lastTimeUsed, right.lastTimeUsed)
+        })
+
+        usageStats.forEach {
+            Log.e("ThreeO", "패키지명: ${it.packageName}, lastTimeUsed: ${Date(it.lastTimeUsed)}, " +
+                    "totalTimeInForeground: ${it.totalTimeInForeground}")
+        }
+    }
+
+    private fun getAppUsageStats(): MutableList<UsageStats> {
+        val cal = Calendar.getInstance()
+        cal.add(Calendar.YEAR, -1)
+
+        val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val queryUsageStats = usageStatsManager
+            .queryUsageStats(
+                UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis,
+                System.currentTimeMillis()
+            )
+        return queryUsageStats
+    }
+
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
+        return mode == AppOpsManager.MODE_ALLOWED
     }
 }
