bf4cd2f11fe953c488e75e89b1e19c5c4564159a
jiwoo <hjw6112@gmail.com>
2021-09-26 16:53:18 +0900

    TermsOfUesActivity.kt/ add Activity

---
+++ app/src/main/java/com/example/threeo/TermsOfUesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.threeo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsOfUesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_ues)
+    }
+}
